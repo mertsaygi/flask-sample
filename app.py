2eb97c73ad8481c6ebde4ecd1778aab34f061372
Mert Saygı <mertsaygi@users.noreply.github.com>
2021-08-27 03:49:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/foo")
+def hello_foo():
+    return "<p>Hello from foo!</p>"
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
